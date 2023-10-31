b0321ed7add11cd8a098c460f613f478c3e462ba
Donatien de La Chesnais <donatien.houitte_de_la_chesnais@etu.minesparis.psl.eu>
2023-10-31 16:56:28 +0100

    Je récupère le big indice

--- Projet_happiness.py
+++ Projet_happiness.py
@@ -8,12 +8,32 @@ df = pd.read_csv(url)
 
 import numpy as np
 
-#fonctions d'affichage des graphes
+##fonctions d'affichage des graphes
+
+#pour plusieurs indices d'un pays
 import matplotlib.pyplot as plt
-def graph(pays, indice, *autres_indices):
+def graphindice(pays, indice, *autres_indices):
     masque=df['Country name']==pays
     sub_df=df[[indice, *autres_indices , "year",'Country name']]         #on crée la dataframe qui nous intéresse
     sub_df[masque].plot(x="year")
     plt.show()
 
-graph("Albania","Social support", "Perceptions of corruption","Freedom to make life choices")
+graphindice("Albania","Social support", "Perceptions of corruption","Freedom to make life choices")
+
+#pour plusieurs pays mais 1 indice
+def graphpays(indice, p1, p2):
+    masque=(df['Country name']==p1) | (df['Country name']==p2)
+    sub_df=df[[indice, "year",'Country name']]         #on crée la dataframe qui nous intéresse
+    sub_df[masque].plot(x="year")
+    plt.show()
+
+graphpays("Social support","Albania","Afghanistan")
+
+#histogramme de la répartition du big indice
+def histbi(année):
+    masque=df['year']==année
+    sub_df=df[["big indice", "year",'Country name']]         #on crée la dataframe qui nous intéresse
+    sub_df[masque].hist(x="big indice")
+    plt.show()
+
+histbi(2000)
