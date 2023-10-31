297e6e57bfe53162942697ff72a42386aff95728
Donatien de La Chesnais <donatien.houitte_de_la_chesnais@etu.minesparis.psl.eu>
2023-10-31 15:08:03 +0100

    Fonction d'affichage des graphs incomplete

--- Projet_happiness.py
+++ Projet_happiness.py
@@ -1,1 +1,11 @@
 #Ceci est le script commun
+import numpy as np
+
+
+#fonctions d'affichage des graphes
+import matplotlib.pyplot as plt
+def graph(pays, indice):
+    #on cherche d'abord les indices correspndant au pays 
+
+    sub_df=df.loc[f"{pays}",[f"{indice}","year"]]             #on crée la dataframe qui nous intéresse seulement
+    sub_df.plot()
